0f6e54b94ced137d9d32d6f6f6fd21c75f1b7089
domingoc1232 <152648268+domingoc1232@users.noreply.github.com>
2025-12-15 17:48:09 -0700

    updates to work best with tmux

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -5,6 +5,6 @@ return {
     require('Comment').setup()
     -- Use Ctrl+/ instead 
     vim.keymap.set('n', '<leader>c', '<Plug>(comment_toggle_linewise_current)', { desc = 'Toggle comment' })
-    vim.keymap.set('v', '<leader>c', '<Plug>(comment_toggle_linewise_visual)', { desc = 'Toggle comment' })   
+    vim.keymap.set('v', '<leader>c', '<Plug>(comment_toggle_linewise_visual)', { desc = 'Toggle comment' })
   end,
 }
